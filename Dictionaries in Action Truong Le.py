9c2201d0ea437ffbd724e78d446354169d55fc72
Truong Nguyen Le <truong.le@stu.jefferson.kyschools.us>
2018-09-18 08:30:59 -0400

    dictionary

--- Dictionaries in Action Truong Le.py
+++ Dictionaries in Action Truong Le.py
@@ -2,8 +2,9 @@
 #Dictionaries in Action
 import random
 
+#username pick
 username = input('What is your username? ')
-
+#weapons
 musket = {
     'speed': 60,
     'power': 90,
@@ -20,12 +21,58 @@ flamegun = {
     'speed': 80,
     'power': 70,
 }
+#weapon pick
 print('musket, throwing_knife, shotgun, flamegun')
 weapon = eval(input('What is your choice of weapon? '))
-
+#player stats
 game_stats = {
     'user': username,
     'health': 100,
     'xp': 0,
     'weapons': weapon,
     }
+#monsters
+monster1 = {
+    'speed': 50,
+    'power': random.choices(range(10, 16)),
+    'health': 50,
+}
+monster2 = {
+    'speed': 60,
+    'power': random.choices(range(15, 21)),
+    'health': 80,
+}
+monster3 = {
+    'speed': 70,
+    'power': random.choices(range(30, 41)),
+    'health': 100,
+}
+#bosses
+boss1 = {
+    'speed': 50,
+    'power': random.choices(range(10, 21)),
+    'health': 70,
+}
+boss2 = {
+    'speed': 70,
+    'power': random.choices(range(20, 31)),
+    'health': 80,
+}
+boss3 = {
+    'speed': 90,
+    'power': random.choices(range(10, 80)),
+    'health': 150,
+}
+#missrate
+missrate = ['hit', 'miss']
+misses = random.choices[missrate]
+
+
+
+
+
+
+
+
+
+    
